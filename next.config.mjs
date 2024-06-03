208fe7ecceb4398320b152217ecc1909dd3bd818
augustocesardamasceno <augustoccmd@hotmail.com>
2024-06-02 21:23:58 -0300

    Adicionando configurações ao next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,50 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/api/auth/callback/google',
+        destination: '/api/auth/callback/google',
+        permanent: true,
+      },
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+        ],
+      },
+    ]
+  },
+  experimental: {
+    serverActions: true,
+  },
+  future: {
+    webpack5: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
