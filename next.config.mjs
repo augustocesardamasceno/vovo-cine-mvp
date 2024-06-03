a08aab2c792b462b96192a132e00207b89b87f6e
augustocesardamasceno <augustoccmd@hotmail.com>
2024-06-03 10:28:52 -0300

    Desfazendo alterações no config

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin',
-          },
-        ],
-      },
-    ];
-  },
-  experimental: {
-    serverActions: true,
-  },
-  future: {
-    webpack5: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-      };
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
