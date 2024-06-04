0f328109200d3a134242f14f1baf73c8a7e8b401
augustocesardamasceno <augustoccmd@hotmail.com>
2024-06-03 21:27:32 -0300

    Ajsutando next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_JWT_SECRET: process.env.NEXTAUTH_JWT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  },
 };
 
 export default nextConfig;
