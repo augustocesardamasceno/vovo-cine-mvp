281d7f09601ac5d275dfa89a702818aa4cb9bfd6
augustocesardamasceno <augustoccmd@hotmail.com>
2024-06-04 08:31:25 -0300

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
   env: {
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_JWT_SECRET: process.env.NEXTAUTH_JWT_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   },
